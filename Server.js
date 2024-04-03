126d4e09525cff024041f8bdbe1701790f02df47
its-pz-mir <usmanshouket256@gmail.com>
2024-04-03 15:31:22 +0500

    cors setting

--- Server.js
+++ Server.js
@@ -7,7 +7,14 @@ const cors = require('cors');
 const router = require('./Routes');
 const cookieParser = require('cookie-parser');
 
-app.use(cors());
+app.use(
+    cors({
+        origin: ["https://foodapp-backend-production.up.railway.app/"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", router);
